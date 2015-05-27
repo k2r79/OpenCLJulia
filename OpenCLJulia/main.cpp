60cc944358b3d2f206ae070335619299e8a4eeb5
Vincent Kelleher <k2r79@hotmail.fr>
2015-05-27 09:43:00 +0200

    Initial Commit

---
+++ OpenCLJulia/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCLJulia
+//
+//  Created by Vincent Kelleher on 27/05/2015.
+//  Copyright (c) 2015 Vincent Kelleher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
